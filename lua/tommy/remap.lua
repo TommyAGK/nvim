adc7cbae7327e6d74565c2013602988cf2718df8
Tommy Asmund Gunnar Kristiansen <tommy.kristiansen@basefarm-orange.com>
2023-06-29 10:59:03 +0200

    ready

---
+++ lua/tommy/remap.lua
@@ -0,0 +1,22 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- copy paste
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- format buffer with lsp
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+
+-- map Ctrl-c to esc lol
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+-- make file executable
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- space space is the same as :so which sources active file
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
