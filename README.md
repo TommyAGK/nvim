6edf275923e0e7ac03ff92bfcc74f2df490b4653
Tommy Asmund Gunnar Kristiansen <tommy.kristiansen@basefarm-orange.com>
2024-01-18 17:18:13 +0100

    added a few plugins, wrote a readme

---
+++ README.md
@@ -0,0 +1,100 @@
+# Comfy Neovim
+
+This is a setup of neovim i find comfy to work in.
+There are parts collected from various other users, inspired by or simply self-invented
+
+I thank every single plugin writer for their efforts, without you. I would not be comfy.
+
+
+# Hotkey overview!
+Common things
+
+Leader = spacebar
+
+leader pv = vim built in file explorer
+
+ctrl-c = escape while in insert mode 
+
+leader x = makes file executable by calling a shell command silently
+           this sets the +x chmod flag on the open file
+
+leader leader = sources the current open file
+
+## Copy paste things
+
+leader p 
+works in select mode, deletes the selected stuff without putting in a register
+and pasted what you got in your paste register
+
+leader y
+works in normal and visual mode, yanks to "+y register
+
+leader Y
+works in normal and visual, yanks to "+Y register
+
+## Formatting stuff
+
+leader f
+Uses the language server to format according to the language
+
+## harpoon keybinds: 
+### normal mode:
+
+leader a = append current file to harpoon list, 
+CTRL-e = open harpoon list via telescope plugin
+CTRL-h = quick select element 1 in the harpoon list while visible 
+CTRL-t = quick select element 2 in the harpoon list while visible
+CTRL-n = quick select element 3 in the harpoon list while visible
+CTRL-s = quick select element 4 in the harpoon list while visible
+
+These are when you dont have the CTRL-e window open, quick jumps!
+CTRL-SHIFT-P = Previous buffer in the list
+CTRL-SHIFT-N = Next buffer in the list 
+
+## telescope keybinds
+
+### normal:
+leader pf = find files
+ctrl-p = find files in current git repo
+leader ps = grep for word in files
+
+## undo
+leader u = opens up the UI for undotree, use window motion to interact
+
+## fugitive (git stuff)
+leader gs = git, i dont really use this much.
+
+## Language server stuff
+these only work if a LSP is loaded, 
+ie, you have a file open in a language you have an LSP for
+
+### normal: 
+
+gd = go to definition
+K = open manual entry for element under cursor
+
+leader vws = workspace symbols
+leader vd = vim diagnostics
+[d = next diagnostic element
+]d = previous diagnostic element
+leader vca = code action
+leader vrr = code references
+leader vrn = code rneame
+
+### insert:
+CTRL-h = signature help
+CTRL-p = select previous cmp mapping
+CTRL-n = select next cmp mapping
+CTRL-y = accept currently selected cmp mapping
+
+## Trouble (bugfixing setup)
+### normal:
+leader xq = opens up the trouble window (toggle)w
+
+
+## Zen mode
+### normal:
+leader zz = 90 width zen mode (toggle), numbers.
+leader zZ = 80 widht zen mode (toggle), no numbers
+
+
--- lua/storm/packer.lua
+++ lua/storm/packer.lua
@@ -1,50 +1,77 @@
--- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use({ 'rose-pine/neovim', as = 'rose-pine' })
+    -- fuzzy finder
 
-  vim.cmd('colorscheme rose-pine')
-
-  use (
-        'nvim-treesitter/nvim-treesitter',
-	{  run = ':TSUpdate'
-    })
-    use ( 'nvim-treesitter/playground')
-    use { 'theprimeagen/harpoon', branch = "harpoon2", requires = {{"nvim_lua/plenary.nvim"}}}
-    use ( 'mbbill/undotree')
-    use ( 'tpope/vim-fugitive')
     use {
-          'VonHeikemen/lsp-zero.nvim',
-           branch = 'v3.x',
-  requires = {
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},             -- Required
-    {                                      -- Required
-      'williamboman/mason.nvim',
-     -- run = function()
-     --   pcall(vim.cmd, 'MasonUpdate')
-     -- end,
-    },
-    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},     -- Required
-    {'hrsh7th/cmp-nvim-lsp'}, -- Required
-    {'hrsh7th/cmp-buffer'}, -- Required
-    {'hrsh7th/cmp-nvim-lua'}, -- Required
-    {'L3MON4D3/LuaSnip'},     -- Required
-    {'saadparwaiz1/cmp_luasnip'}, -- Required
-    {'rafamadriz/friendly-snippets'}, -- Required
+    -- theme
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
+    })
 
-  }
-}
+    -- A pretty list for showing diagnostics, references,
+    -- telescope results, quickfix and location lists to
+    -- help you solve all the trouble your code is causing.
+    use({
+        "folke/trouble.nvim",
+        config = function()
+            require("trouble").setup {
+                icons = false
+            }
+        end
+    })
+
+    -- highlighting of code and stuff
+    use(
+        'nvim-treesitter/nvim-treesitter',
+        { run = ':TSUpdate'
+        })
+    use('nvim-treesitter/playground')                                                          -- shows the code as seen by treesitter
+    use {'theprimeagen/harpoon', branch = "harpoon2"} -- clever jumping
+    use('mbbill/undotree')                                                                     -- bigass undo system
+    use('tpope/vim-fugitive')                                                                  -- git plugin
+
+
+    -- Beginning language Server stuff
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },   -- Required
+            { 'williamboman/mason.nvim' }, -- Required
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' }, -- Required
+            { 'hrsh7th/cmp-buffer' }, -- Required
+            { 'hrsh7th/cmp-path' }, -- Required
+            { 'hrsh7th/cmp-nvim-lua' }, -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' }, -- Required
+
+            -- snippets
+            { 'saadparwaiz1/cmp_luasnip' }, -- Required
+            { 'rafamadriz/friendly-snippets' }, -- Required
+
+        }
+    }
+    -- end of language server stuff
+
+    -- silly stuff
+    use { 'eandrju/cellular-automaton.nvim' }
+    -- focus you bastard
+    use {'folke/zen-mode.nvim'}
 end)
