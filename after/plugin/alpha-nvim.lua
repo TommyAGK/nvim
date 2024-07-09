dedd4cccdf1761443a42cf152b41c18a4f1b7adb
Tommy Asmund Gunnar Kristiansen <tommy.agk@gmail.com>
2024-07-09 09:44:31 +0200

    fixed shitty project call

--- after/plugin/alpha-nvim.lua
+++ after/plugin/alpha-nvim.lua
@@ -1,40 +1,39 @@
 local status_ok, alpha = pcall(require, "alpha")
 if not status_ok then
-	return
+    return
 end
 
 local dashboard = require("alpha.themes.dashboard")
 
 dashboard.section.header.val = {
-	[[  /     /|\    /~\  |~\  |\  /| ]],
-	[[ /     / | \  /   \ |  \ | \/ | ]],
-	[[/___  /  |  \ \   / |  / | /\ | ]],
-	[[   /     |     \ /  |_/  |/  \| ]],
-	[[  /      |     / \  | \  |    | ]],
-	[[ /       |    /   \ |  \ |    | ]],
+    [[  /     /|\    /~\  |~\  |\  /| ]],
+    [[ /     / | \  /   \ |  \ | \/ | ]],
+    [[/___  /  |  \ \   / |  / | /\ | ]],
+    [[   /     |     \ /  |_/  |/  \| ]],
+    [[  /      |     / \  | \  |    | ]],
+    [[ /       |    /   \ |  \ |    | ]],
 }
 dashboard.section.buttons.val = {
-	dashboard.button("f", " Find file", ":Telescope find_files <CR>"),
-	dashboard.button("e", " New file", ":ene <BAR> startinsert <CR>"),
-	-- TODO fix this shitty "Find Projects"
-	dashboard.button("p", " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
-	dashboard.button("r", " Recent files", ":Telescope oldfiles <CR>"),
-	dashboard.button("t", " Find text", ":Telescope live_grep <CR>"),
-	dashboard.button("c", " Config", ":Telescope find_files cwd=~/.config/nvim/ <CR>"),
-	dashboard.button("u", " Update", ":Lazy<CR>"),
-	dashboard.button("q", " Quit", ":qa<CR>"),
+    dashboard.button("f", " Find file", ":Telescope find_files <CR>"),
+    dashboard.button("e", " New file", ":ene <BAR> startinsert <CR>"),
+    dashboard.button("p", " Find project", ":Telescope projects <CR>"),
+    dashboard.button("r", " Recent files", ":Telescope oldfiles <CR>"),
+    dashboard.button("t", " Find text", ":Telescope live_grep <CR>"),
+    dashboard.button("c", " Config", ":Telescope find_files cwd=~/.config/nvim/ <CR>"),
+    dashboard.button("u", " Update", ":Lazy<CR>"),
+    dashboard.button("q", " Quit", ":qa<CR>"),
 }
 
 local function footer()
-	return [[Why problem make, when no problem have, don't want?
+    return [[Why problem make, when no problem have, don't want?
                        -- Tommy]]
 end
 
 local os = vim.loop.os_uname().sysname
 if os == "Windows_NT" then
-	--dashboard.section.buttons.val[6] = dashboard.button("c", " Edit nvim configs", ":e ~/AppData/Local/nvim/ <CR>")
-	dashboard.section.buttons.val[6] =
-		dashboard.button("c", " Edit nvim configs", ":Telescope find_files cwd=~/AppData/Local/nvim/ <CR>")
+    --dashboard.section.buttons.val[6] = dashboard.button("c", " Edit nvim configs", ":e ~/AppData/Local/nvim/ <CR>")
+    dashboard.section.buttons.val[6] =
+        dashboard.button("c", " Edit nvim configs", ":Telescope find_files cwd=~/AppData/Local/nvim/ <CR>")
 end
 
 dashboard.section.footer.val = footer()
