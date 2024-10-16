e697000a18ab9103fc8fe51ff0e85df9ab77c058
Tommy Asmund Gunnar Kristiansen <Tommy.agk@gmail.com>
2024-10-16 15:47:50 +0200

    Updated witch-key spec

--- after/plugin/which-key.lua
+++ after/plugin/which-key.lua
@@ -4,73 +4,117 @@ local ts = require("telescope.builtin")
 -- which-key defaults to normal mode
 
 -- telescope parts
-wk.register({
-	["<leader>p"] = {
-		name = "telescope file browser",
-		f = { ts.find_files, "find files" },
-		g = { ts.git_files, "find files in git repo" },
-		s = {
-			function()
-				ts.grep_string({ search = vim.fn.input("find word greplike > ") })
-			end,
-			"Search for text",
-		},
-	},
+-- wk.register({
+-- 	["<leader>p"] = {
+-- 		name = "telescope file browser",
+-- 		f = { ts.find_files, "find files" },
+-- 		g = { ts.git_files, "find files in git repo" },
+-- 		s = {
+-- 			function()
+-- 				ts.grep_string({ search = vim.fn.input("find word greplike > ") })
+-- 			end,
+-- 			"Search for text",
+-- 		},
+-- 	},
+-- })
+
+wk.add({
+    { "<leader>p",  group = "Telescope file browser" },
+    { "<leader>pf", ts.find_files,                   desc = "Find files" },
+    { "<leader>pg", ts.git_files,                    desc = "Find files in git repo" },
+    {
+        "<leader>ps",
+        function()
+            ts.grep_string({ search = vim.fn.input("find word greplike > ") })
+        end,
+        desc = "Search for text"
+    },
 })
 
 -- harpoon parts
 local harpoon = require("harpoon")
 
-wk.register({
-	["<C>"] = {
-		name = "Harpoon stuff",
-		e = {
-			"view harpoon list",
-		},
-		h = {
-			"view harpoon list",
-		},
-		t = {
-			"view harpoon list",
-		},
-		n = {
-			"view harpoon list",
-		},
-		s = {
-			"view harpoon list",
-		},
-	},
+wk.add({
+    { "<C>",  group = "Harpoon stuff" },
+    { "<C>e", desc = "View Harpoon list" },
+    { "<C>h", desc = "View Harpoon list" },
+    { "<C>n", desc = "View Harpoon list" },
+    { "<C>s", desc = "View Harpoon list" },
+    { "<C>t", desc = "View Harpoon list" },
 })
-wk.register({
-	["<C-S>"] = {
-		P = {
-			"Previous buffer in harpoon",
-		},
-		N = {
-			"Next buffer in harpoon",
-		},
-	},
+
+wk.add({
+    { "<C-S>N", desc = "Next buffer in harpoon" },
+    { "<C-S>P", desc = "Previous buffer in harpoon" },
 })
+
+--wk.register({
+--    ["<C>"] = {
+--        name = "Harpoon stuff",
+--        e = {
+--            "view harpoon list",
+--        },
+--        h = {
+--            "view harpoon list",
+--        },
+--        t = {
+--            "view harpoon list",
+--        },
+--        n = {
+--            "view harpoon list",
+--        },
+--        s = {
+--            "view harpoon list",
+--        },
+--    },
+--})
+-- wk.register({
+--     ["<C-S>"] = {
+--         P = {
+--             "Previous buffer in harpoon",
+--         },
+--         N = {
+--             "Next buffer in harpoon",
+--         },
+--     },
+-- })
+
 -- ssh stuff
-wk.register({
-	["<leader>rf"] = {
-		f = { "Remote file actions" },
-	},
+--wk.register({
+--    ["<leader>rf"] = {
+--        f = { "Remote file actions" },
+--    },
+--})
+
+wk.add({
+    { "<leader>rff", desc = "Remote file actions" },
 })
 
 -- LSP stuff
-wk.register({
-	["K"] = { "Get info on element" },
+wk.add({
+    { "K", desc = "Get info on element" },
 })
 
+--wk.register({
+--    ["K"] = { "Get info on element" },
+--})
+
 -- generic leader namings
-wk.register({
-	["<leader>"] = {
-		a = { "Add file to harpoon jump list" },
-		r = { "Remote host" },
-		g = { "Git" },
-		d = { "Dashboard" },
-		Z = { "Zen" },
-		z = { "LSP" },
-	},
+wk.add({
+    { "<leader>Z", desc = "Zen" },
+    { "<leader>a", desc = "Add file to harpoon jump list" },
+    { "<leader>d", desc = "Dashboard" },
+    { "<leader>g", desc = "Git" },
+    { "<leader>r", desc = "Remote Host" },
+    { "<leader>z", desc = "LSP" },
 })
+-- wk.register({
+--     ["<leader>"] = {
+--         a = { "Add file to harpoon jump list" },
+--         r = { "Remote host" },
+--         g = { "Git" },
+--         d = { "Dashboard" },
+--         Z = { "Zen" },
+--         z = { "LSP" },
+--     },
+-- })
